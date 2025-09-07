3f0bd7f06f1578137703233e9c3ec27ba30307a9
Tamim Khan <md.tamim.iq.khan@gmail.com>
2025-09-07 21:26:14 +0600

    added choose section with some functionalities and responsive design

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log('Hello, World');
+const loadCategories = () => {
+   fetch('https://openapi.programming-hero.com/api/categories')
+     .then(res => res.json())
+     .then(json => displayCategory(json.categories))
+}
+
+const displayCategory = (arr) => {
+  const categoryContainer = document.getElementById('categoryContainer');
+  arr.forEach((elm) => {
+    categoryContainer.innerHTML += `
+      <button class="btn btn-ghost text-sm font-normal active:bg-[#15803d] active:text-white">${elm.category_name}</button>
+    `;
+  });
+}
+
+const toggleHistory = () => {
+  const historySec = document.getElementById('historySec');
+
+  historySec.classList.toggle('hidden');
+}
+
+loadCategories();
